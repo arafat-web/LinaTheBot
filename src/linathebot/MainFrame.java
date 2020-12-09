3582e53b5a2b61f89c9c21b303cf151fcb11be1f
Arafat Hossain Ar <arafat.122260@gmail.com>
2020-12-09 22:04:07 +0600

    various changes

--- src/linathebot/MainFrame.java
+++ src/linathebot/MainFrame.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Author: Arafat Hossain Ar
+Date: 09/12/2020
+Project Name: Lina The Talking Bot
  */
 package linathebot;
 
